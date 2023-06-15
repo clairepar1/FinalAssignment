096fcf1075f7cb73f2c3c4c347836fa9545ec42d
Clairee <klearetipar@hotmail.com>
2023-06-15 14:56:05 +0300

    Assignment...

--- assignment.py
+++ assignment.py
@@ -1,12 +1,14 @@
+import matplotlib.pyplot as plt
 import pandas as pd
-import numpy
-import matplotlib
+import numpy as np
+
 df = pd.read_csv("Healthcare - Case Studies 3  country population growth rate 2016-2019.csv")
 
-df['zip_code'] = df ['zip_code'].astype(int)
+df["zip_code"] = df ["zip_code"].astype(int)
 bottle_per_zip = df.groupby(["zip_code"])["bottles_sold"].max().sort_values(ascending=False)
-print(bottle_per_zip)
 
-
-
-
+plt.scatter(bottle_per_zip.index, bottle_per_zip, c=np.random.rand(len(bottle_per_zip),3))
+plt.title("Bottles Sold")
+plt.xlabel("Zip Code")
+plt.ylabel("Bottles Sold")
+plt.show()
